c7b352c4dbb7da96cca87728d2db950f7186d809
michele camera <michy.cam95@gmail.com>
2022-04-28 16:55:22 +0200

    modify font-size all p elements

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ import Skillsection from "@/components/Skillsection.vue";
 import Trustedsection from "@/components/Trustedsection.vue";
 import Footer from "@/components/Footer.vue";
 import "@fontsource/montserrat";
-import "@fortawesome/fontawesome-svg-core";
 
 export default {
   name: "App",
@@ -53,6 +52,9 @@ body {
   list-style: none;
   text-decoration: none;
   display: flex;
+}
+*p {
+  font-size: 0.9rem;
 }
 *h6 {
   color: orange;
